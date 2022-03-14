c949ea11de7ff41674c8c9c6b8998cf9134c601b
AnonBD <99282155+anonbd1@users.noreply.github.com>
2022-03-15 00:21:07 +0600

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 	/* Check Login form submitted */	
 	if(isset($_POST['Submit'])){
 		/* Define username and associated password array */
-		$logins = array('Anonbd' => 'hackerbd','username1' => 'password1','username2' => 'password2');
+		$logins = array('HackerBD' => '123456','username1' => 'password1','username2' => 'password2');
 		
 		/* Check and assign submitted Username and Password to new variable */
 		$Username = isset($_POST['Username']) ? $_POST['Username'] : '';
